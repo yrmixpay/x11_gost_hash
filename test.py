7d054ac40ed61241eb6c20a5dc5a1407907e8da5
yrmixpay <49076502+yrmixpay@users.noreply.github.com>
2019-04-13 02:21:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,16 +3,16 @@ from binascii import unhexlify, hexlify
 
 import unittest
 
-# Sibcoin block #1
-# ivan@b1:~/.sibcoin$ sibcoin-cli getblockhash 1
-# 0000085b23aeab914465a41fc753d61e0e39ca05d5870e77f6287763928cd9b0
-# ivan@b1:~/.sibcoin$ sibcoin-cli getblock 0000085b23aeab914465a41fc753d61e0e39ca05d5870e77f6287763928cd9b0
+# Yrmixcoin block #0
+# yrmix@b1:~/.syrmixcoin$ yrmixcoin-cli getblockhash 0
+# 00000d87443123cc4848baa166279698b0eb529698c4440ff85cf03ca94dff12
+# yrmix@b1:~/.yrmixcoin$ yrmixcoin-cli getblock 00000d87443123cc4848baa166279698b0eb529698c4440ff85cf03ca94dff12
 #{
-#    "hash" : "0000085b23aeab914465a41fc753d61e0e39ca05d5870e77f6287763928cd9b0",
-#    "confirmations" : 351337,
-#    "size" : 186,
-#    "height" : 1,
-#    "version" : 3,
+#    "hash": "00000d87443123cc4848baa166279698b0eb529698c4440ff85cf03ca94dff12",
+#    "confirmations": 1,
+#    "size": 288,
+#    "height": 0,
+#    "version": 1,
 #    "merkleroot" : "8db686f7eabfe0ba61e2e0601f3bd557a5926da4f68fa96fe1dafa5b98980c91",
 #    "tx" : [
 #        "8db686f7eabfe0ba61e2e0601f3bd557a5926da4f68fa96fe1dafa5b98980c91"
@@ -21,7 +21,7 @@ import unittest
 #    "nonce" : 569046,
 #    "bits" : "1e0ffff0",
 #    "difficulty" : 0.00024414,
-#    "chainwork" : "0000000000000000000000000000000000000000000000000000000000200020",
+#    "merkleroot": "2e3875684e7dbaa1b97a91aad29f0a7c2633b2c9f81401a9ebd326a08b42489d",
 #    "previousblockhash" : "00000c492bf73490420868bc577680bfc4c60116e7e85343bc624787c21efa4c",
 #    "nextblockhash" : "00000722c462136d5a41d80f3a7f36f24777e4276b7e141933649fc47dddd071"
 #}
