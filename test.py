f6a899ae6818a60830ae02aab5c5b92eec01c843
yrmixpay <49076502+yrmixpay@users.noreply.github.com>
2019-04-14 02:30:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from binascii import unhexlify, hexlify
 import unittest
 
 # Yrmixcoin block #0
-# yrmix@b1:~/.syrmixcoin$ yrmixcoin-cli getblockhash 0
+# yrmix@b1:~/.yrmixcoin$ yrmixcoin-cli getblockhash 0
 # 00000d87443123cc4848baa166279698b0eb529698c4440ff85cf03ca94dff12
 # yrmix@b1:~/.yrmixcoin$ yrmixcoin-cli getblock 00000d87443123cc4848baa166279698b0eb529698c4440ff85cf03ca94dff12
 #{
@@ -13,14 +13,14 @@ import unittest
 #    "size": 288,
 #    "height": 0,
 #    "version": 1,
-#    "merkleroot" : "8db686f7eabfe0ba61e2e0601f3bd557a5926da4f68fa96fe1dafa5b98980c91",
+#    "merkleroot" : "2e3875684e7dbaa1b97a91aad29f0a7c2633b2c9f81401a9ebd326a08b42489d",
 #    "tx" : [
-#        "8db686f7eabfe0ba61e2e0601f3bd557a5926da4f68fa96fe1dafa5b98980c91"
+#        "2e3875684e7dbaa1b97a91aad29f0a7c2633b2c9f81401a9ebd326a08b42489d"
 #    ],
-#    "time" : 1431130209,
-#    "nonce" : 569046,
+#    "time" : 1553886060,
+#    "nonce" : 594160,
 #    "bits" : "1e0ffff0",
-#    "difficulty" : 0.00024414,
+#    "difficulty" : 0.000244140625,
 #    "merkleroot": "2e3875684e7dbaa1b97a91aad29f0a7c2633b2c9f81401a9ebd326a08b42489d",
 #    "previousblockhash" : "00000c492bf73490420868bc577680bfc4c60116e7e85343bc624787c21efa4c",
 #    "nextblockhash" : "00000722c462136d5a41d80f3a7f36f24777e4276b7e141933649fc47dddd071"
@@ -33,7 +33,7 @@ header_hex = ("03000000" +
     "f0ff0f1e" +
     "d6ae0800")
 
-best_hash = 'b0d98c92637728f6770e87d505ca390e1ed653c71fa4654491abae235b080000'
+best_hash = '00000d87443123cc4848baa166279698b0eb529698c4440ff85cf03ca94dff12'
 
 class TestSequenceFunctions(unittest.TestCase):
 
