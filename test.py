5fe3cc56205ced38a6abe5c1adf875daabd1dc9a
yrmixpay <yrmixpay@gmail.com>
2019-04-17 02:55:25 +0300

    update

--- test.py
+++ test.py
@@ -3,30 +3,30 @@ from binascii import unhexlify, hexlify
 
 import unittest
 
-# Yrmixcoin block #0
-# yrmix@b1:~/.yrmixcoin$ yrmixcoin-cli getblockhash 0
-# 00000d87443123cc4848baa166279698b0eb529698c4440ff85cf03ca94dff12
-# yrmix@b1:~/.yrmixcoin$ yrmixcoin-cli getblock 00000d87443123cc4848baa166279698b0eb529698c4440ff85cf03ca94dff12
+# yrmixcoin block #1
+# ivan@b1:~/.yrmixcoin$ yrmixcoin-cli getblockhash 1
+# 0000024a1ede3e643c6beabfbffef7d0be6f96175d870f524671f46ca9bc8edd
+# ubuntu@b1:~/.yrmixcoin$ yrmixcoin-cli getblock 0000024a1ede3e643c6beabfbffef7d0be6f96175d870f524671f46ca9bc8edd
 #{
-#    "hash": "00000d87443123cc4848baa166279698b0eb529698c4440ff85cf03ca94dff12",
-#    "confirmations": 1,
-#    "size": 288,
-#    "height": 0,
-#    "version": 1,
-#    "merkleroot" : "2e3875684e7dbaa1b97a91aad29f0a7c2633b2c9f81401a9ebd326a08b42489d",
+#    "hash" : "0000024a1ede3e643c6beabfbffef7d0be6f96175d870f524671f46ca9bc8edd",
+#    "confirmations" : 1,
+#    "size" : 179,
+#    "height" : 1,
+#    "version" : 20000000,
+#    "merkleroot" : "044cbb591e616d75def4e8e62f6c8443624f8aba9e3a62e62e5ff1c5c8c59f71",
 #    "tx" : [
-#        "2e3875684e7dbaa1b97a91aad29f0a7c2633b2c9f81401a9ebd326a08b42489d"
+#        "044cbb591e616d75def4e8e62f6c8443624f8aba9e3a62e62e5ff1c5c8c59f71"
 #    ],
-#    "time" : 1553886060,
-#    "nonce" : 594160,
+#    "time" : 1555423404,
+#    "nonce" : 7189,
 #    "bits" : "1e0ffff0",
-#    "difficulty" : 0.000244140625,
-#    "merkleroot": "2e3875684e7dbaa1b97a91aad29f0a7c2633b2c9f81401a9ebd326a08b42489d",
+#    "difficulty" : 000244140625,
+#    "chainwork" : "0000000000000000000000000000000000000000000000000000000000200020",
 #    "previousblockhash" : "00000c492bf73490420868bc577680bfc4c60116e7e85343bc624787c21efa4c",
-#    "nextblockhash" : "00000722c462136d5a41d80f3a7f36f24777e4276b7e141933649fc47dddd071"
+#    "nextblockhash" : "00000d87443123cc4848baa166279698b0eb529698c4440ff85cf03ca94dff12"
 #}
 
-header_hex = ("03000000" +
+header_hex = ("02000000" +
     "4cfa1ec2874762bc4353e8e71601c6c4bf807657bc6808429034f72b490c0000" +
     "910c98985bfadae16fa98ff6a46d92a557d53b1f60e0e261bae0bfeaf786b68d"
     "61504d55" +
